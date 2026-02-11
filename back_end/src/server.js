0b1955d776b240a43921c7c2eb80eddcbef52727
Keith-Smith12 <keithsimth23@gmail.com>
2026-02-11 17:37:47 +0100

    back

---
+++ back_end/src/server.js
@@ -0,0 +1,20 @@
+const { WebSocketServer } = require('ws');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
+
+wss.on('connection', (ws) => {
+
+  ws.on('error',(error) => {
+    console.error('WebSocket error:', error);
+  });
+  
+ws.on('message', (data) => {
+ws.send(data);
+
+});
+
+});
