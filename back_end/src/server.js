ee9d859afe5f053c7253ae1076030377d44624df
Keith-Smith12 <keithsimth23@gmail.com>
2026-02-13 12:22:58 +0100

    deploy

--- back_end/src/server.js
+++ back_end/src/server.js
@@ -7,16 +7,15 @@ dotenv.config();
 const wss = new WebSocketServer({ port: process.env.PORT || 3000 });
 
 wss.on('connection', (ws) => {
-
-  ws.on('error',(error) => {
+  ws.on('error', (error) => {
     console.error('WebSocket error:', error);
   });
 
-ws.on('message', (data) => {
+  ws.on('message', (data) => {
+    wss.clients.forEach((client) => {
+      client.send(data.toString());
+    });
+  });
 
-  ws.clients.forEach((client) => (clients.send(data.toString())));
-
-});
-
-console.log("connected");
+  console.log("connected");
 });
